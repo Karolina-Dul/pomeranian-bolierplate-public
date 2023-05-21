4e9a9c87cef28871b41808a3eb4a8a217c710f34
KarolinaDuljas <duljas.karolina@gmail.com>
2023-05-21 12:24:14 +0200

    if, switch, logic operators

---
+++ src/App/Exercises/Js/Exercise-js-booleans/router-data.jsx
@@ -0,0 +1,10 @@
+import { Exercise } from './exercise-js-booleans';
+
+export const blockRouterMetaData = {
+  path: 'exercise-js-booleans',
+  date: null,
+  linkLabel: 'Js - booleans',
+  blockNo: 18,
+  element: <Exercise />,
+  tags: ['booleans', 'js', 'let', 'const'],
+};
