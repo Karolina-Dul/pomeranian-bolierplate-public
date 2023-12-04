c950446f77f07b4792bfafcc471dfc969fb5c6e5
KarolinaDuljas <duljas.karolina@gmail.com>
2023-12-04 18:06:48 +0100

    Dodana wersja tabletowa i poprawiony nieco żart Norrisa

--- src/App/Dashboard/MyData/MyData.jsx
+++ src/App/Dashboard/MyData/MyData.jsx
@@ -2,15 +2,17 @@ export const MyData = () => {
   return (
     <div className="dashboard-about-me">
       <div className="person"></div>
-      <p className="name">Karolina Duljas</p>
-      <div className="description">
-        <p>Gdańsk</p>
-        <p>
-          e-mail: <br></br> duljas.karolina@gmail.com
-        </p>
-        <p>
-          telefon: <br></br> 517100536
-        </p>
+      <div className="person-data">
+        <p className="name">Karolina Duljas</p>
+        <div className="description">
+          <p>Gdańsk</p>
+          <p>
+            e-mail: <br></br> duljas.karolina@gmail.com
+          </p>
+          <p>
+            telefon: <br></br> 517100536
+          </p>
+        </div>
       </div>
     </div>
   );
