8a76a8eb548d087b37a7d801b82b78816dbc555a
KarolinaDuljas <duljas.karolina@gmail.com>
2023-12-06 21:00:54 +0100

    FAQ - poprawki 2

--- src/App/Components/SingleQuestion/SingleQuestion.jsx
+++ src/App/Components/SingleQuestion/SingleQuestion.jsx
@@ -6,23 +6,10 @@ export const FaqQuestions = (props) => {
   const { question, answer } = props;
 
   const [isQuestionClicked, setQuestionClicked] = useState(false);
-  // const [arrowDown, setArrowDown] = useState(false);
-
-  // const ifArrow = ({ active, <Arrow/> }) => (
-  //   <div className={`banner ${active ? "active" : "inactive"}`}>{}</div>
-  // )
 
   function clickAnswer() {
     setQuestionClicked(!isQuestionClicked);
   }
-
-  // function changeArrow() {
-  //   if (setArrowDown(!arrowDown)) {
-  //     <button className='activeArrow'><Arrow /></button>
-  //   } else {
-  //     <button className='inactiveArrow'><Arrow /></button>
-  //   }
-  // }
 
   return (
     <div className="border-open">
